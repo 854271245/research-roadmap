4bc36c2b69823afc85a9c4811a0cb245f12d061b
854271245 <854271245@qq.com>
2026-05-26 21:57:30 +0800

    Add files via upload

---
+++ research-roadmap/week1/hello.py
@@ -0,0 +1,9 @@
+print("hello research")
+
+name = "Peng"
+major = "Management Science and Engineering"
+direction = "Data Science and Intelligent Decision Making"
+
+print(name)
+print(major)
+print(direction)
